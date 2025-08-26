7b53168fac0f21eb02ef1f0251922a729103d970
brotigen23 <brotigen23@gmail.com>
2025-08-26 19:30:28 +0300

    components

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -1,16 +1,17 @@
 import type {  Task } from "../types";
-
 import { BASE_URL } from "../config";
+
+
 
 export async function getTasks(projectID: number): Promise<Task[]>{
     const result = await fetch(`${BASE_URL}/tasks?projectId=${projectID}`);
     if (!result.ok){
         throw new Error("Не удалось загрузить задачи")
     }
-    return result.json()
+    return result.json();
 }
 
-export async function addTask(input: {projectID: number, title: string, done?: boolean}):Promise<Task> {
+export async function addTask(input: {projectId: number, title: string, done?: boolean}):Promise<Task> {
     const result = await fetch(`${BASE_URL}/tasks`, 
         {
             method: 'POST',
@@ -21,5 +22,41 @@ export async function addTask(input: {projectID: number, title: string, done?: b
     if (!result.ok){
         throw new Error("Не удалось создать задачу")
     }
-    return result.json()
+    return result.json();
 }
+
+export async function updateTask(taskID: number, task: Partial<Task>): Promise<Task>{
+    const result = await fetch(`${BASE_URL}/tasks/${taskID}`,
+         {
+            method: 'PATCH',
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(task)
+        }
+    );
+    if (!result.ok){
+        throw new Error("Не удалось обновить задачу")
+    }
+
+    return result.json();
+}
+
+export async function deleteTask(taskID: number): Promise<Task>{
+    const result = await fetch(`${BASE_URL}/tasks/${taskID}`,
+         {
+            method: 'DELETE'
+        }
+    );
+    if (!result.ok){
+        throw new Error("Не удалось удалить задачу")
+    }
+
+    return result.json();
+}
+
+// TODO:
+// confirm prompt alert
+export async function putTask(): Promise<Task>{
+}
+
+// TODO:
+export async function renameTask(): Promise<Task>{}
--- src/components/TaskComponents.tsx
+++ src/components/TaskComponents.tsx
@@ -1,6 +1,6 @@
 import { createResource, createSignal, For, Show, type Accessor } from 'solid-js'
 import type { Task } from '../types'
-import { getTasks, addTask } from '../api/tasks';
+import { getTasks, addTask, updateTask, deleteTask } from '../api/tasks';
 import type { JSX } from 'solid-js/h/jsx-runtime';
 
 export type TaskListProps = {
@@ -8,10 +8,32 @@ export type TaskListProps = {
 }
 
 export default function TaskList(props: TaskListProps){
-    const [tasks] = createResource<Task[], number | null>(props.projectID, (id) => getTasks(id as number));
+    const [tasks, {refetch}] = createResource<Task[], number | null>(props.projectID, (id) => getTasks(id as number));
     const [title, setTitle] = createSignal<string>("");
+    const [submiting, setSubmiting] = createSignal<boolean>(false);
     const onTitleInput: JSX.EventHandler<HTMLInputElement, InputEvent> = (e) => {setTitle(e?.currentTarget.value)}
-    const onCreate: JSX.EventHandler<HTMLFormElement, SubmitEvent> = (e) => {}
+
+    const onCreate: JSX.EventHandler<HTMLFormElement, SubmitEvent> = async (e) => {
+      e.preventDefault();
+      const projectID = props.projectID();
+      
+      const text = title().trim();
+      if (!projectID || !text) return;
+      try{
+        setSubmiting(true);
+        await addTask({projectId: projectID, title: text});
+
+        setTitle("");
+        await refetch();
+      } finally {
+        setSubmiting(false);
+      }
+    }
+
+    const toggleDone:(task: Task) => void = async (task: Task) => {await updateTask(task.id, {done: !task.done}); refetch()};
+
+    const remove:(task: Task) => void = async (task:Task) => {await deleteTask(task.id); refetch()}
+
     return (
       <section>
         <h2>
@@ -19,19 +41,29 @@ export default function TaskList(props: TaskListProps){
             {(id) => <>Задачи проекта №{id()}</>}
           </Show>
         </h2>
-        <form onsubmit={()}></form>
+        <form onsubmit={onCreate}>
+          <input type='text' placeholder='Добавить задачу' value={title()} oninput={onTitleInput} disabled={!props.projectID() || submiting()}/>
+          <button type='submit' disabled={!props.projectID() || submiting()}>
+            Добавить
+          </button>
+        </form>
         <Show when={props.projectID} fallback={<p>Пока не выбран проект</p>}>
           <Show when={!tasks.loading} fallback={<p>Загружаем задачи...</p>}>
             <Show when={!tasks.error} fallback={<p>Ошибка:{String(tasks.error)}</p>}>            
               <ul>
                 <For each={tasks() ?? []}>
                   {(task) => (
+                    <>
                     <li>
-                      <input type='checkbox' checked={task.done} />
+                      <input type='checkbox' checked={task.done} onchange={() => toggleDone(task)} />
                       <span>
                         {task.title}
                       </span>
-                    </li>
+                    </li>                    
+                    <button type='button' onclick={() => remove(task)}>
+                      Удалить
+                    </button>
+                    </>
                   )}
                 </For>
               </ul>
